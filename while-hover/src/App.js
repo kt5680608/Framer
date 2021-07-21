5082beab555d90b5ac8825ec257895f69b9b53a8
kt5680608 <kt5680608>
2021-07-21 21:51:58 +0900

    whileHover

---
+++ while-hover/src/App.js
@@ -0,0 +1,22 @@
+import { motion } from 'framer'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <motion.div 
+        className = "motion-div"
+        whileHover = {{
+          rotate: 360,
+          transition: {
+            repeat: Infinity,
+            repeatType: 'reverse',
+            duration: 2
+          }
+        }}>
+      </motion.div>
+    </div>
+  );
+}
+
+export default App;
